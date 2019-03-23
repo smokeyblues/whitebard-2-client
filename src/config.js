a6cfe6839ed0704757c73ae7ca20c0cc4c13b349
smokeyblues <smokeyblues@CORP.AMAZONWORKSPACES.COM>
2019-03-23 11:47:34 -0600

    configuring environments

--- src/config.js
+++ src/config.js
@@ -1,16 +1,44 @@
-export default {
+const dev = {
     s3: {
         REGION: "us-west-2",
-        BUCKET: "whitebard-csv"
+        BUCKET: "whitebard-2-api-dev-whitebardcsvbucket-1dks629tyksia"
     },
     apiGateway: {
         REGION: "us-west-2",
-        URL: "https://l9aqth4s74.execute-api.us-west-2.amazonaws.com/prod"
+        URL: "https://qrgl7r3bqi.execute-api.us-west-2.amazonaws.com/dev"
     },
     cognito: {
         REGION: "us-west-2",
-        USER_POOL_ID: "us-west-2_fge29WBRo",
-        APP_CLIENT_ID: "6utenh1nmv7mjiqhae3s8r5u2k",
-        IDENTITY_POOL_ID: "us-west-2:62eacbb3-ce8b-4130-bb5c-5b70a28ba554"
+        USER_POOL_ID: "us-west-2_Xaykl5D2f",
+        APP_CLIENT_ID: "5nbaidql1ca86queejb9h9e7nh",
+        IDENTITY_POOL_ID: "us-west-2:eb2f368a-dabf-48fa-80f3-2001868e5123"
     }
+};
+
+const prod = {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "whitebard-2-api-prod-whitebardcsvbucket-1kqz2w2er97uu"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://m0ddae4gcb.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_tsxUun5Mr",
+        APP_CLIENT_ID: "4b9tnqmfgnqranqva85e8vlmkf",
+        IDENTITY_POOL_ID: "us-west-2:14bd4faf-e3ea-446c-a274-d88013a0f2a4"
+    }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
